59d0e3dcbb73f938b37a3945c16c3c787180b9e9
LucasCasadoPrime <lucas.casado@epitech.eu>
2022-06-22 14:23:12 +0200

    [master] => add pop nod and add node between two nodes

--- src/linked_list.c
+++ src/linked_list.c
@@ -41,6 +41,16 @@ node_t *push_back_ll(node_t *head, void* data)
     return(head);
 }
 
+// add node between two nodes
+node_t *add_node_ll(node_t *head, void* data, node_t *prev, node_t *next)
+{
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+    new_node->data = data;
+    new_node->next = next;
+    prev->next = new_node;
+    return(head);
+}
+
 // remove the first node of the list
 node_t *pop_front_ll(node_t *head)
 {
@@ -61,6 +71,20 @@ node_t *pop_back_ll(node_t *head)
     return(head);
 }
 
+// remove a node from the list
+node_t *remove_ll(node_t *head, void* data)
+{
+    node_t *tmp = head;
+    while (tmp->next != NULL) {
+        if (tmp->next->data == data) {
+            tmp->next = tmp->next->next;
+            return(head);
+        }
+        tmp = tmp->next;
+    }
+    return(head);
+}
+
 // print the linked_list
 node_t *print_ll(node_t *list) {
     node_t *tmp = list;
@@ -71,16 +95,4 @@ node_t *print_ll(node_t *list) {
     return list;
 }
 
-// int main(){
-//     node_t *list = NULL;
-
-//     list = push_back_ll(list, "Hello");
-//     list = push_front_ll(list, "World");
-//     list = push_back_ll(list, "!");
-//     list = pop_front_ll(list);
-//     list = pop_back_ll(list);
-
-//     print_ll(list);
-
-//     return 0;
-// }
+// Source of help : https://www.codegrepper.com/code-examples/c/linked++list+in+c
